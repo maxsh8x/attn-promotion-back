28bfe6df2e016ee005d0893f5aaa56b70caf3e6d
max <maksim.sharnin@gmail.com>
2017-10-19 12:34:29 +0300

    WIP

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'answer-promotion-back',
+      script    : 'build/index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'deploy',
+      host : '192.168.1.8',
+      ref  : 'origin/master',
+      repo : 'git@gitlab.com:thesalt/answer-promotion-back.git',
+      path : '/home/deploy/projects/answer-promotion-back',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'deploy',
+      host : '192.168.1.8',
+      ref  : 'origin/master',
+      repo : 'git@gitlab.com:thesalt/answer-promotion-back.git',
+      path : '/home/deploy/projects/answer-promotion-back',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
