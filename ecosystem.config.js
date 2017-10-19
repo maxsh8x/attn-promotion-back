e85fa5b104095f86cf66cc5ef7cb99f84f4ca72b
max <maksim.sharnin@gmail.com>
2017-10-19 14:14:46 +0300

    deploy fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       name      : 'answer-promotion-back',
       script    : 'build/index.js',
-      exec_interpreter: '~/.nvm/versions/node/v8.7.0/bin/node',
+      exec_interpreter: '/home/deploy/.nvm/versions/node/v8.7.0/bin/node',
       env: {
         COMMON_VARIABLE: 'true'
       },
